45d07af0333b240363262117e11dc7411e8dad85
1nanonan1 <nouk3@horizon.csueastbay.edu>
2025-09-22 17:39:12 -0700

    Update Adapter.java

--- Adapter.java
+++ Adapter.java
@@ -1,18 +1,70 @@
-public class Adapter extends BaseAdapter {
-    @Override public int getCount() {
-      return 0;
+package com.example.flashcards;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FlashcardAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Flashcard> flashcardList;
+    private LayoutInflater inflater;
+
+    public FlashcardAdapter(Context context, List<Flashcard> flashcardList) {
+        this.context = context;
+        this.flashcardList = flashcardList;
+        this.inflater = LayoutInflater.from(context);
     }
-  
-    @Override public Object getItem(int i) {
-      return null;
-    }
-  
-    @Override public long getItemId(int i) {
-      return 0;
-    }
-  
+
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public int getCount() {
+        return flashcardList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return flashcardList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if(convertView == null) {
+            convertView = inflater.inflate(R.layout.item_flashcard, parent, false);
+            holder = new ViewHolder();
+            holder.textView = convertView.findViewById(R.id.cardText);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Flashcard flashcard = flashcardList.get(position);
+        holder.textView.setText(flashcard.getQuestion());
+
+        // toggle Q/A when clicked
+        convertView.setOnClickListener(v -> {
+            if(holder.textView.getText().equals(flashcard.getQuestion())) {
+                holder.textView.setText(flashcard.getAnswer());
+            } else {
+                holder.textView.setText(flashcard.getQuestion());
+            }
+        });
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView textView;
     }
 }
